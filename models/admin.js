82a3ee4a130d16a840239c8b1204a9db1bd6e356
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2018-12-31 11:45:05 -0800

    New admin routing added to API.

---
+++ models/admin.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const Image = require('./Images/image.routes');
+const Construction = require('./Constructions/construction.routes');
+const Type = require('./Types/type.routes');
+
+router.use('/type', Type);
+
+router.use('/construction', Construction);
+
+router.use('/image', Image);
+
+router.use('/', ( req, res ) => {
+  res.status(404).send();
+});
+
+module.exports = router;
