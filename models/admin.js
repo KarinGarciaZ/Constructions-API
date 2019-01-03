90171463089d7d2cdeba9a6a18423455c5049d33
Oscar Jovanny García Zepeda <oscarin962011@hotmail.com>
2019-01-03 15:50:28 -0800

    Added User model to the project.

--- models/admin.js
+++ models/admin.js
@@ -4,12 +4,15 @@ const router = express.Router();
 const Image = require('./Images/image.routes');
 const Construction = require('./Constructions/construction.routes');
 const Type = require('./Types/type.routes');
+const User = require('./Users/user.routes');
 
 router.use('/type', Type);
 
 router.use('/construction', Construction);
 
 router.use('/image', Image);
+
+router.use('/user', User);
 
 router.use('/', ( req, res ) => {
   res.status(404).send();
